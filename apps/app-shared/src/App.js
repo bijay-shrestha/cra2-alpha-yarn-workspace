0209280b5fdcb1742fa8a622e03f828d2d9a23ae
MikeSuiter <msuiter@workpoint.com>
2018-11-15 14:23:32 -0600

    Changed @customer to @company

--- apps/app-shared/src/App.js
+++ apps/app-shared/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Header from '@customer/core/components/Header';
+import Header from '@company/core/components/Header';
 
 class App extends Component {
   render() {
--- packages/core/components/Header/index.js
+++ packages/core/components/Header/index.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import Link from '@customer/shared/components/Link';
+import Link from '@company/shared/components/Link';
 import logo from './logo.svg';
 import Hello from '../Hello';
 
