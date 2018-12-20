6e6655c20a8c54e2bc8a537a38c353e8abf87842
MikeSuiter <msuiter@workpoint.com>
2018-12-20 08:58:45 -0600

    Added unused for lint testing.

--- apps/app-shared/src/App.js
+++ apps/app-shared/src/App.js
@@ -4,6 +4,8 @@ import Header from '@company/core/src/components/Header';
 
 class App extends Component {
   render() {
+    const unused = true; // <- Here to test lint warning
+
     return (
       <div className="App">
         <Header title="CRA2 with Yarn Workspace & Monorepo" />
